3a8522f77f56e2e10b6227e47ac3f68f33127a40
ahsancrazy <67188978+ahsancrazy@users.noreply.github.com>
2022-12-27 17:31:58 +0500

    Add files via upload
    new report enhancement changes

---
+++ report_inhancement/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Report Inhancement',
+    'version': '13.0.0.3',
+    'category': 'Report Inhancement',
+    "description": """ Report Inhancement """,
+    'author': "IDT",
+    'website': 'http://www.infinitedt.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'base','account','sale','purchase','stock'
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'reports/report_inhancement.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
